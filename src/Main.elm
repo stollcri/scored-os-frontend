f384f7f041cba32332a9da71284bfa9d74aab54b
Christopher Stoll <stollcri@gmail.com>
2018-06-07 22:51:39 -0400

    Add team name

--- src/Main.elm
+++ src/Main.elm
@@ -1,43 +1,56 @@
 module Main exposing (..)
 
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, Attribute, button, div, input, text)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick, onInput)
 
 
-main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+main = 
+    Html.beginnerProgram { model = model, view = view, update = update }
 
 
 -- MODEL
 
-type alias Model = Int
+type alias Model =
+    { team: String
+    , score: Int
+    }
 
 model : Model
 model =
-  0
+    { team = "Team A"
+    , score = 0
+    }
 
 
 -- UPDATE
 
-type Msg = Increment | Decrement
+type Msg
+    = Name String
+    | Increment
+    | Decrement
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Name name ->
+            { model | team = name }
 
-    Decrement ->
-      model - 1
+        Increment ->
+            { model | score = 1 }
+
+        Decrement ->
+            { model | score = 0 }
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ input [ type_ "text", placeholder "Team A", onInput Name ] []
+        , button [ onClick Decrement ] [ text "-" ]
+        , div [] [ text (toString model.score) ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
     
