a69bdd7a440bb58118c60135be9287dd91a58896
Christopher Stoll <stollcri@gmail.com>
2018-07-08 20:48:46 -0400

    Add elm-bootstrap and basic styling

--- src/View.elm
+++ src/View.elm
@@ -1,6 +1,11 @@
 module View exposing (..)
 
-import Html exposing (Html, Attribute, a, br, button, div, input, span, text)
+import Bootstrap.Button as Button
+import Bootstrap.Grid as Grid
+import Bootstrap.Form as Form
+import Bootstrap.Form.Input as Input
+import Bootstrap.Form.InputGroup as InputGroup
+import Html exposing (Html, Attribute, a, br, button, div, h1, input, span, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
 import RemoteData
@@ -26,40 +31,67 @@ page model  =
         NotFoundRoute ->
             notFoundView
 
-broadcastView: Game -> Html Msg
-broadcastView game =
-    div []
-        [ scorebox TeamA game.teamA
-        , scorebox TeamB game.teamB
-        , div []
-            [ br [] []
-            , a [ href "#games/1" ] [ text "games/1" ]
-            , br [] []
-            , a [ href "#games/2" ] [ text "games/2" ]
+viewheader : Html Msg
+viewheader =
+    Grid.row []
+        [ Grid.col []
+            [ h1 []
+                [ text "Mark-a-Score"
+                ]
             ]
         ]
 
-scorebox : Team -> TeamData -> Html Msg
-scorebox team teamData =
-    div []
-        [ input
-            [ type_ "text"
-            , value teamData.name
-            , onInput (Name team)
-            , placeholder "Add team name"
-            , class "form-control"
-            ] []
-        , div [ class "input-group" ]
-            [ div [ class "input-group-prepend" ]
-                [ button [ onClick (Score team -1), class "btn btn-outline-secondary" ]
-                    [ span [ class "glyphicon glyphicon-minus" ] [ text "-" ]
+broadcastView: Game -> Html Msg
+broadcastView game =
+    Grid.container []
+        [ viewheader
+        , scoreboard TeamA game.teamA
+        , scoreboard TeamB game.teamB
+        , Grid.row []
+            [ Grid.col []
+                [ Form.form []
+                    [ a [ href "#games/1" ] [ text "games/1" ]
                     ]
                 ]
-            , input [ type_ "text", value (toString teamData.score), class "form-control" ] []
-            , div [ class "input-group-prepend" ]
-                [ button [ onClick (Score team 1), class "btn btn-outline-secondary" ]
-                    [ span [ class "glyphicon glyphicon-plus" ] [ text "+" ]
+            ]
+        , Grid.row []
+            [ Grid.col []
+                [ a [ href "#games/2" ] [ text "games/2" ] ]
+            ]
+        ]
+
+scoreboard : Team -> TeamData -> Html Msg
+scoreboard team teamData =
+    Grid.row []
+        [ Grid.col []
+            [ Form.group []
+                [ Input.text
+                    [ Input.attrs
+                        [ placeholder "Add team name"
+                        , value teamData.name
+                        , onInput (Name team)
+                        ]
                     ]
+                , InputGroup.config
+                    ( InputGroup.text
+                        [ Input.attrs [ value (toString teamData.score) ]
+                        ]
+                    )
+                    |> InputGroup.predecessors
+                        [ InputGroup.button
+                            [ Button.danger
+                            , Button.attrs [ onClick (Score team -1) ]
+                            ]
+                            [ text "-"]
+                        ]
+                    |> InputGroup.successors
+                        [ InputGroup.button
+                            [ Button.success
+                            , Button.attrs [ onClick (Score team 1) ]
+                            ]
+                            [ text "+"]
+                        ]
+                    |> InputGroup.view
                 ]
             ]
         ]
@@ -74,13 +106,52 @@ tuneInView model id =
             text "Loading ..."
 
         RemoteData.Success game ->
-            div []
-                [ scorebox TeamA game.teamA
+            Grid.container []
+                [ viewheader
+                , scorebox TeamA game.teamA
                 , scorebox TeamB game.teamB
                 ]
 
         RemoteData.Failure err ->
             text (toString err)
+
+scorebox : Team -> TeamData -> Html Msg
+scorebox team teamData =
+    Grid.row []
+        [ Grid.col []
+            [ Form.group []
+                [ Input.text
+                    [ Input.attrs
+                        [ value teamData.name
+                        , disabled True
+                        ]
+                    ]
+                , InputGroup.config
+                    ( InputGroup.text
+                        [ Input.attrs
+                            [ value (toString teamData.score)
+                            , disabled True
+                            ]
+                        ]
+                    )
+                    |> InputGroup.predecessors
+                        [ InputGroup.button
+                            [ Button.danger
+                            , Button.attrs [ disabled True ]
+                            ]
+                            [ text "-"]
+                        ]
+                    |> InputGroup.successors
+                        [ InputGroup.button
+                            [ Button.success
+                            , Button.attrs [ disabled True ]
+                            ]
+                            [ text "+"]
+                        ]
+                    |> InputGroup.view
+                ]
+            ]
+        ]
 
 notFoundView : Html Msg
 notFoundView = 
